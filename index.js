878b55cf208edd818b5c86079c480af9d1b93085
fábio thompson <93395738+Poke-bits@users.noreply.github.com>
2022-12-27 12:01:47 -0300

    editando o index

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import { enviarEmail } from './enviarEmail.js'
     nodeCron.schedule('*/2 * * * *', () => {
         const credenciais= logins.pop()
         console.log('começou porra dig diow')
-        enviarEmail(credenciais.email, "seu login venuz produção", `seu login venuz é ${credenciais.email} e sua senha é ${credenciais.senha},\n você pode acessar pelo link https://venuz.app/login \n
-        qualquer dúvida me chamar no teams! \n
+        enviarEmail(credenciais.email, "seu login example produção", `seu login example é ${credenciais.email} e sua senha é ${credenciais.senha},\n você pode acessar pelo link https://example.app/login \n
+        qualquer dúvida me chamar no discord! \n
         Att, \n
         Fábio Thompson\n
-        Favor não responder esse email, qualquer dúvida pode me chamar no teams!` )
+        Favor não responder esse email, qualquer dúvida pode me chamar no discord!` )
         console.log(`email enviado para ${credenciais.email}`)
     
     });
